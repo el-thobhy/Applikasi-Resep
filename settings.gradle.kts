1ac75129be9bcc830a20b0a10d8d7c6edb1207db
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2023-10-04 22:22:57 +0700

    first init modul

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Applikasi Resep"
+include(":app")
+include(":core")
+include(":bookmark")
